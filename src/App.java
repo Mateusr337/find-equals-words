6631d97e30f5d04f4999438c7cae9d3c5b5a6190
mateus <mateusc.rossetto@hotmail.com>
2023-01-12 13:04:49 -0300

    feat: resolve code

--- src/App.java
+++ src/App.java
@@ -1,9 +1,12 @@
-import java.util.ArrayList;
 import java.util.List;
 
 public class App {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
         final String[] arrOne = new String[]{"mateus", "apple", "orange", "strawberry", "mistake", "jedi", "fill"};
         final String[] arrTwo = new String[]{"six", "apple", "million", "worse", "better", "jedi", "rose"};
+
+        FindWords findWords = new FindWords();
+        List<String> result = findWords.sameWords(arrOne, arrTwo);
+        System.out.println("The same words are: " + result);
     }
 }
