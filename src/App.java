8902f8db4638f0c409c1d011aa7a323c7bab30a7
mateus <mateusc.rossetto@hotmail.com>
2023-01-12 11:58:04 -0300

    refactor: find words method

--- src/App.java
+++ src/App.java
@@ -1,5 +1,9 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        
+        final String[] arrOne = new String[]{"mateus", "apple", "orange", "strawberry", "mistake", "jedi", "fill"};
+        final String[] arrTwo = new String[]{"six", "apple", "million", "worse", "better", "jedi", "rose"};
     }
 }
--- src/FindWords.java
+++ src/FindWords.java
@@ -1,13 +1,28 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
-public final class FindWords {
-    public static void sameWords (String[] arrOne, String[] arrTwo) {
-        for (int i = 0; i < arrOne.length; i++) {
-            for (int j = 0; j < arrTwo.length; j++) {
-                if (Objects.equals(arrOne[i], arrTwo[j])) {
-                    System.out.println(arrOne[i]);
-                }
-            }
+
+public class FindWords {
+
+    private ArrayList<String> commonWords = new ArrayList<>();
+    
+    public List<String> sameWords (String[] arrOne, String[] arrTwo) {
+        for (String word : arrOne) {
+            boolean found = tryFindWord(word, arrTwo);
+            if (found) pushWord(word);
         }
+        return commonWords;
+    }
+
+    private boolean tryFindWord (String word, String[] arr) {
+        for (String wordArr : arr) {
+            if (Objects.equals(word, wordArr)) return true;
+        }
+        return false;
+    }
+
+    private void pushWord(String word) {
+        commonWords.add(word);
     }
 }
